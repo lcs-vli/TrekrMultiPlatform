3d31a06186ceb8d9c8b74609d9578b5e6d66cf7d
Victoria Li <VLi@lcs.on.ca>
2021-01-25 14:58:29 -0500

    Initial Commit

---
+++ Shared/TrekrMultiPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrMultiPlatformApp.swift
+//  Shared
+//
+//  Created by Li, Muchen on 2021/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TrekrMultiPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
