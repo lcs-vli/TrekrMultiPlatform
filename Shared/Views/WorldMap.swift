5cea4e881918e7174ba02670a5dca870c25629b5
Victoria Li <VLi@lcs.on.ca>
2021-01-25 20:20:05 -0500

    added annotations to map

--- Shared/Views/WorldMap.swift
+++ Shared/Views/WorldMap.swift
@@ -9,14 +9,29 @@ import SwiftUI
 
 struct WorldMap: View {
     
+    //get a referecen to the location store
+    //this is a derived value passed as a parameter
+    @ObservedObject var store: LocationStore
+    
     //centre on LCS, wide enough to show most of eastern Nother America
     @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 44.43922, longitude: -78.26571),
                                            span: MKCoordinateSpan(latitudeDelta: 40, longitudeDelta: 40))
     
     var body: some View {
         
-        Map(coordinateRegion: $region)
-            .navigationTitle("Map")
+        Map(coordinateRegion: $region, annotationItems: store.places) { location in
+            
+            MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)) {
+                
+                Image(location.country)
+                    .resizable()
+                    .cornerRadius(10)
+                    .frame(width: 80, height: 40)
+                    .shadow(radius: 3)
+            }
+            
+        }
+        .navigationTitle("Map")
     
     }
 }
@@ -24,7 +39,7 @@ struct WorldMap: View {
 struct WorldMap_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            WorldMap()
+            WorldMap(store: testStore)
         }
     }
 }
