f8cddbecde5882283710f9c9765dad570fddeae5
Victoria Li <VLi@lcs.on.ca>
2021-01-25 20:12:34 -0500

    got the map view centered in lakefield

--- Shared/Views/WorldMap.swift
+++ Shared/Views/WorldMap.swift
@@ -4,17 +4,27 @@
 //
 //  Created by Li, Muchen on 2021/1/25.
 //
-
+import MapKit
 import SwiftUI
 
 struct WorldMap: View {
+    
+    //centre on LCS, wide enough to show most of eastern Nother America
+    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 44.43922, longitude: -78.26571),
+                                           span: MKCoordinateSpan(latitudeDelta: 40, longitudeDelta: 40))
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Map(coordinateRegion: $region)
+            .navigationTitle("Map")
+    
     }
 }
 
 struct WorldMap_Previews: PreviewProvider {
     static var previews: some View {
-        WorldMap()
+        NavigationView {
+            WorldMap()
+        }
     }
 }
