3c6f10d8fe7934c4783d0aa9ac2e16718835dae9
Victoria Li <VLi@lcs.on.ca>
2021-01-25 19:21:49 -0500

    added location type

--- Shared/TrekrMultiPlatformApp.swift
+++ Shared/TrekrMultiPlatformApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TrekrMultiPlatformApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LocationDetail()
         }
     }
 }
---
+++ Shared/Views/LocationDetail.swift
@@ -0,0 +1,22 @@
+//
+//  LocationView.swift
+//  TrekrMultiPlatform
+//
+//  Created by Li, Muchen on 2021/1/25.
+//
+
+import SwiftUI
+
+struct LocationDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            LocationDetail()
+        }
+    }
+}
