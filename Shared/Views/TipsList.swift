992a15a80ff74ce4060b6b6d85e843bca91475c7
Victoria Li <VLi@lcs.on.ca>
2021-01-25 20:07:54 -0500

    added tips and map view, and tab bar to switch between them

---
+++ Shared/Views/TipsList.swift
@@ -0,0 +1,20 @@
+//
+//  TipsList.swift
+//  TrekrMultiPlatform
+//
+//  Created by Li, Muchen on 2021/1/25.
+//
+
+import SwiftUI
+
+struct TipsList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TipsList_Previews: PreviewProvider {
+    static var previews: some View {
+        TipsList()
+    }
+}
---
+++ Shared/Views/WorldMap.swift
@@ -0,0 +1,20 @@
+//
+//  WorldMap.swift
+//  TrekrMultiPlatform
+//
+//  Created by Li, Muchen on 2021/1/25.
+//
+
+import SwiftUI
+
+struct WorldMap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WorldMap_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldMap()
+    }
+}
